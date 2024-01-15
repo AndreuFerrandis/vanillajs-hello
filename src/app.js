295428891b80b6060928563149bd8f0e30408d46
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-01-15 16:17:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const excusas = [
+  "Se me olvidó",
+  "No recibí el mensaje",
+  "Tuve problemas técnicos",
+  "Tenía una reunión urgente",
+  "Mi perro se comió la tarea",
+  "Hubo un corte de electricidad",
+  "Mi despertador no sonó",
+  "Estaba en una llamada importante",
+  "Se me acabó la batería del teléfono",
+  "Estaba atrapado en el tráfico"
+];
+
+function generarExcusa() {
+  const indice = Math.floor(Math.random() * excusas.length);
+
+  const excusaElemento = document.getElementById("excusa");
+  excusaElemento.textContent = "Esta es mi excusa favorita: " + excusas[indice];
+}
+
+generarExcusa();
